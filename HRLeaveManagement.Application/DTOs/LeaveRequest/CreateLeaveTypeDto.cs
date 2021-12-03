bb1c92240b713ca39077a534c8518e7a508250b2
poorkarbhushan1987 <poorkarbhushan1987@gmail.com>
2021-12-03 17:32:08 +0530

    Mediator and CQRS patterns

---
+++ HRLeaveManagement.Application/DTOs/LeaveRequest/CreateLeaveTypeDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HRLeaveManagement.Application.DTOs.LeaveRequest
+{
+    public class CreateLeaveTypeDto
+    {
+        public string Name { get; set; }
+        public int DefaultDays { get; set; }
+    }
+}
---
+++ HRLeaveManagement.Application/Features/LeaveTypes/Requests/Commands/CreateLeaveTypeCommand.cs
@@ -0,0 +1,14 @@
+﻿using HRLeaveManagement.Application.DTOs;
+using HRLeaveManagement.Application.DTOs.LeaveRequest;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HRLeaveManagement.Application.Features.LeaveTypes.Requests.Commands
+{
+    public class CreateLeaveTypeCommand : IRequest<int>
+    {
+        public CreateLeaveTypeDto LeaveTypeDto { get; set; }
+    }
+}
